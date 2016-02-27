415a3efe2871bb74a2bb4f0827dbf7baecf2ddba
zhmz90 <zhmz90@gmail.com>
2016-02-27 20:16:40 +0800

    Finished first problem store credit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # GoogleCodeJam
 
 Julia codes of google code jam
+
+### For New Competitors
+- [x] Store Credit
+- [ ] Reverse Words
--- src/ForNewCompetitors/StoreCredit.jl
+++ src/ForNewCompetitors/StoreCredit.jl
@@ -1,32 +1,36 @@
 module StoreCredit
 
 function sc()
-    num_testcase = readline(STDIN) |> x->strip(x) |> x->parse(Int64,x)
-
+    fl = open("A-large-practice.in")
+    num_testcase = readline(fl) |> x->strip(x) |> x->parse(Int64,x)
+    outfl = open("large_storecredit.out","w")
     i=0
     while (num_testcase>=1)
         i+=1
-        c  = readline(STDIN) |> x->strip(x) |> x->parse(Int64,x)
-        l  = readline(STDIN) |> x->strip(x) |> x->parse(Int64,x)
-        ps = readline(STDIN) |> x->strip(x) |> x->split(x, " ") |> x->map(y->parse(Int64,y),x)
-        findinds(c,l,ps,i)
+        c  = readline(fl) |> x->strip(x) |> x->parse(Int64,x)
+        l  = readline(fl) |> x->strip(x) |> x->parse(Int64,x)
+        ps = readline(fl) |> x->strip(x) |> x->split(x, " ") |> x->map(y->parse(Int64,y),x)
+        findinds(outfl,c,l,ps,i)
         num_testcase -= 1
     end
-    
+    close(outfl)
+    close(fl)
 end
 
-function findinds(c,l,ps,i)
+function findinds(outfl,c,l,ps,idcase)
     @assert length(ps) == l
-    sort!(ps)
+    indperm = sortperm(ps)
+    ps = ps[indperm]
+    ind1,ind2=0,0
     for i in 1:l-1
         for j in i+1:l
             s = ps[i]+ps[j]
             if s == c
-                ind1,ind2 = i,j
+                ind1,ind2 = sort([indperm[i],indperm[j]])
             end
         end
     end
-    println("Case #$(i): $ind1 $ind2")
+    println(outfl, "Case #$(idcase): $ind1 $ind2")
 end
 
 
